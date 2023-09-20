9c95b86c8f3718e0f148015a4775d06c872cc6b8
zyfayu <c.longbuankhenn@gmail.com>
2023-09-20 21:54:31 +0800

    Added getter and toString method/s on Class: Book

--- src/Book.java
+++ src/Book.java
@@ -9,6 +9,23 @@ class Book {
 	public Book(int id, String title, String author) {
 		this.id = id;
 	    this.title = title;
-        this.author = author;
-    }
+		this.author = author;
+	}
+	
+	public int getId() {
+	    return id;
+	}
+
+    public String getTitle() {
+	    return title;
+	}
+
+    public String getAuthor() {
+	    return author;
+	}
+
+    @Override
+	public String toString() {
+	    return "Book [id=" + id + ", title=" + title + ", author=" + author + "]";
+	}
 }
