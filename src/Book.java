18703c75b324f6bdce03f973b957470894fedcf0
zyfayu <c.longbuankhenn@gmail.com>
2023-09-20 21:51:56 +0800

    Added Class: Book Added its Constructor

---
+++ src/Book.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Book {
+	private int id;
+    private String title;
+	private String author;
+
+	public Book(int id, String title, String author) {
+		this.id = id;
+	    this.title = title;
+        this.author = author;
+    }
+}
