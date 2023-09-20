19d3ca3012aae9dbc70f61ba89b07788cde56e43
dnledsn <danieledisonyu@gmail.com>
2023-09-20 20:54:59 +0800

    removing from folder

---
+++ Member.java
@@ -0,0 +1,36 @@
+public class Member {
+    private int memberId;
+    private String name;
+    private List<Book> borrowedBooks;
+
+    public Member(int memberId, String name) {
+        this.memberId = memberId;
+        this.name = name;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void borrowBook(Book book) {
+        borrowedBooks.add(book);
+    }
+
+    public void returnBook(Book book) {
+        borrowedBooks.remove(book);
+    }
+
+    @Override
+    public String toString() {
+        return "Member [memberId=" + memberId + ", name=" + name + "]";
+    }
+}
