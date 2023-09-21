91ce45cf012df7dcf60ea456b74250ed112419b2
daine-mabutol <mdcmabutol@gmail.com>
2023-09-21 12:53:57 +0800

    Library.java moved to src folder

---
+++ src/Library.java
@@ -0,0 +1,10 @@
+class Library {
+    private List<Book> books;
+    private List<Member> members;
+
+    public Library() {
+        this.books = new ArrayList<>();
+        this.members = new ArrayList<>();
+    }
+
+}
