719e2cacf161c6e5de0f47ac15ba9481f7f2ed07
rai-gd <raiguiad@gmail.com>
2023-09-23 17:43:49 +0800

    added menu output and input with error handling

---
+++ src/Library.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Library {
+    private List<Book> books;
+    private List<Member> members;
+
+    public Library() {
+        this.books = new ArrayList<>();
+        this.members = new ArrayList<>();
+    }
+
+     public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void addMember(Member member) {
+        members.add(member);
+    }
+
+    public List<Book> getAvailableBooks() {
+        List<Book> availableBooks = new ArrayList<>();
+        for (Book book : books) {
+            if (!isBookBorrowed(book)) {
+                availableBooks.add(book);
+            }
+        }
+        return availableBooks;
+    }
+
+    public boolean isBookBorrowed(Book book) {
+        for (Member member : members) {
+            if (member.getBorrowedBooks().contains(book)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void displayAvailableBooks() {
+        System.out.println("Available Books:");
+        for (Book book : getAvailableBooks()) {
+            System.out.println(book);
+        }
+    }
+
+}
